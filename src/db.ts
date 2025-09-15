642f5f87366ecbd16dd120d1f2a5e141e256675b
tony linux <13antonysun@gmail.com>
2025-09-15 08:34:07 +0530

    initial raw db

---
+++ src/db.ts
@@ -0,0 +1,41 @@
+//INTERACES
+export interface User {
+  id: string;
+  name: string;
+}
+
+export interface Article {
+  id: string;
+  title: string;
+  body: string;
+  authorId: string; // This links to a User
+}
+
+export interface Comment {
+  id: string;
+  text: string;
+  authorId: string; // This links to a User
+  articleId: string; // This links to an Article
+}
+
+//raw data
+const users: User[] = [
+    { id: '1', name: 'Jeff'}, 
+    { id: '2', name: 'Jem'}
+]
+
+const articles: Article[] = [ 
+    {id: 'a1', title: 'How I made Amazong', body: 'Amazong found opens about how he made the biggest online retialer in the world', authorId: '1'},
+    {id: 'a2', title: 'Jem Finch speaks up', body: 'Jem Finch, the kid who was attacked by late Bob Ewell speaks up on what happened on the town paegent day', authorId: '2'},  
+]
+
+const comments: Comment[] = [
+    {id: 'c1', text: 'Nice one Jeff', authorId: '2', articleId: 'a1'},
+    {id: 'c2', text: 'Happy that you survived Jem!', authorId: '1', articleId: 'a2'},
+]
+
+//export
+export const db = {
+    users, articles, comments
+}
+
