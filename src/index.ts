185d0cfbe5f7b82cb66593dd95b97a8e459c68e6
tony linux <13antonysun@gmail.com>
2025-09-15 09:15:32 +0530

    api: route parameters with id

--- src/index.ts
+++ src/index.ts
@@ -7,18 +7,53 @@ const PORT = 3000;
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello TypeScript World!");
 });
-
+//ARTICLES
 app.get("/articles", (req: Request, res: Response) => {
-    res.json(db.articles); 
+  res.json(db.articles);
 });
 
-app.get("/users", (req: Request, res: Response) => {
-    res.json(db.users); 
-})
+//route parameter with id
+app.get("/articles/:id", (req: Request, res: Response) => {
+  const id = req.params.id;
+  const article = db.articles.find((eachArticle) => eachArticle.id === id);
+  if (article) {
+    res.json(article);
+  } else {
+    res.status(404).json({ error: "Article not found" });
+  }
+});
 
+//USERS
+app.get("/users", (req: Request, res: Response) => {
+  res.json(db.users);
+});
+
+//route parameter with id
+app.get("/users/:id", (req: Request, res: Response) => {
+  const id = req.params.id;
+  const user = db.users.find((user) => user.id === id);
+  if (user) {
+    res.json(user);
+  } else {
+    res.status(404).json({ error: "User not found" });
+  }
+});
+
+//COMMENTS
 app.get("/comments", (req: Request, res: Response) => {
-    res.json(db.comments); 
-})
+  res.json(db.comments);
+});
+
+//route parameter with id
+app.get("/comments/:id", (req: Request, res: Response) => {
+    const id = req.params.id; 
+    const comment = db.comments.find(comment => comment.id === id); 
+    if (comment) {
+        res.json(comment); 
+    } else {
+        res.status(404).json({error: "Comment not found"})
+    }
+});
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
