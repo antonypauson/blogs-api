494ce4dc06d8c994b1175d25f05841a5abc8606a
tony linux <13antonysun@gmail.com>
2025-09-16 12:21:53 +0530

    api: patch req for comments,users

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ const app = express();
 const PORT = 3000;
 
 //middlewares
-app.use(express.json()); 
+app.use(express.json());
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello TypeScript World!");
@@ -26,57 +26,58 @@ app.get("/articles/:id", (req: Request, res: Response) => {
   }
 });
 
-//post: new article 
-app.post('/articles', (req: Request, res: Response) => {
+//post: new article
+app.post("/articles", (req: Request, res: Response) => {
   //extract these from the request body
-  const {title, body, authorId} = req.body; 
+  const { title, body, authorId } = req.body;
 
   //make sure its there
   if (!title || !body || !authorId) {
-    return res.status(400).json({error: 'Some data from the client is missing'})
+    return res
+      .status(400)
+      .json({ error: "Some data from the client is missing" });
   }
 
   //create new article object
   const newArticle = {
     id: `a${Date.now()}`, //'a' because we need to use Date.now() as id for others
     title: title,
-    body: body, 
+    body: body,
     authorId: authorId,
-  }; 
+  };
 
-  //add it to db. 
-  db.articles.push(newArticle); 
+  //add it to db.
+  db.articles.push(newArticle);
 
   //201: created
   //then we display teh json of newArticle
-  res.status(201).json(newArticle)
-})
+  res.status(201).json(newArticle);
+});
 
 //path/update an article
-app.patch('/articles/:id', (req: Request, res: Response) => {
+app.patch("/articles/:id", (req: Request, res: Response) => {
   //find the article by id
-  const article = db.articles.find(article => article.id === req.params.id)
+  const article = db.articles.find((article) => article.id === req.params.id);
 
   //make sure it exists
   if (!article) {
-    return res.status(404).json({error: 'Article not found'}); 
+    return res.status(404).json({ error: "Article not found" });
   }
 
   //get the new title or body (or both) from req
-  const {title, body} = req.body; 
+  const { title, body } = req.body;
 
   //update the article
   if (title) {
-    article.title = title; 
+    article.title = title;
   }
   if (body) {
-    article.body = body; 
+    article.body = body;
   }
 
   //response
-  res.json(article); 
-
-})
+  res.json(article);
+});
 
 //USERS
 app.get("/users", (req: Request, res: Response) => {
@@ -95,27 +96,48 @@ app.get("/users/:id", (req: Request, res: Response) => {
 });
 
 //post: new user
-app.post('/users', (req: Request, res: Response) => {
+app.post("/users", (req: Request, res: Response) => {
   //take name from the client
-  const {name} = req.body; 
+  const { name } = req.body;
 
   //verify its there
   if (!name) {
-    return res.status(400).json({error: 'Name is missing'}); 
+    return res.status(400).json({ error: "Name is missing" });
   }
 
   //create new user object
   const newUser = {
-    id: `u${Date.now()}`, 
-    name: name
-  }
+    id: `u${Date.now()}`,
+    name: name,
+  };
 
   //add it
-  db.users.push(newUser); 
+  db.users.push(newUser);
 
   //output + status code
-  res.status(201).json(newUser); 
-})
+  res.status(201).json(newUser);
+});
+
+app.patch("/users/:id", (req: Request, res: Response) => {
+  //which user?
+  const user = db.users.find((user) => user.id === req.params.id);
+
+  //does the user exist?
+  if (!user) {
+    return res.status(404).json({ error: `That user don't exist` });
+  }
+
+  //take data from request
+  const { name } = req.body;
+
+  //update
+  if (name) {
+    user.name = name;
+  }
+
+  //response
+  res.json(user);
+});
 
 //COMMENTS
 app.get("/comments", (req: Request, res: Response) => {
@@ -124,39 +146,61 @@ app.get("/comments", (req: Request, res: Response) => {
 
 //route parameter with id
 app.get("/comments/:id", (req: Request, res: Response) => {
-    const id = req.params.id; 
-    const comment = db.comments.find(comment => comment.id === id); 
-    if (comment) {
-        res.json(comment); 
-    } else {
-        res.status(404).json({error: "Comment not found"})
-    }
+  const id = req.params.id;
+  const comment = db.comments.find((comment) => comment.id === id);
+  if (comment) {
+    res.json(comment);
+  } else {
+    res.status(404).json({ error: "Comment not found" });
+  }
 });
 
 //post: new comment
-app.post('/comments', (req: Request, res: Response) => {
+app.post("/comments", (req: Request, res: Response) => {
   //take data from client
-  const {text, authorId, articleId} = req.body; 
+  const { text, authorId, articleId } = req.body;
 
   //verify its there
   if (!text || !authorId || !articleId) {
-    return res.status(400).json({error: 'Data missing'}); 
+    return res.status(400).json({ error: "Data missing" });
   }
 
   //create comment object
   const newComment = {
-    id: `c${Date.now()}`, 
-    text: text, 
-    authorId: authorId, 
-    articleId: articleId
-  }
+    id: `c${Date.now()}`,
+    text: text,
+    authorId: authorId,
+    articleId: articleId,
+  };
 
   //add it
-  db.comments.push(newComment); 
+  db.comments.push(newComment);
 
   //output + status code
-  res.status(201).json(newComment); 
-})
+  res.status(201).json(newComment);
+});
+
+//update comments
+app.patch("/comments/:id", (req: Request, res: Response) => {
+  //find the comment
+  const comment = db.comments.find((comment) => comment.id === req.params.id);
+
+  //verify
+  if (!comment) {
+    return res.status(404).json({ error: "This comment does not exist" });
+  }
+
+  //get text from request
+  const { text } = req.body;
+
+  //update
+  if (text) {
+    comment.text = text;
+  }
+
+  //response
+  res.json(comment);
+});
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
